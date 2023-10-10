a179487955a0afebe3782a468b6ee87dc1b8c0fe
Dharmendra Sah <dharmendra11250@gmail.com>
2023-10-10 18:41:09 +0300

    initail setup

---
+++ config/settings/env.sample.py
@@ -0,0 +1,64 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = ''
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Application definition
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+MIDDLEWARE += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "database_name",
+        'USER': "database_user",
+        'PASSWORD': "database_password",
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Mailtrap settings
+EMAIL_BACKEND= ''
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = ''
+
+
+# from .base import *
+
+# # SECURITY WARNING: don't run with debug turned on in production!
+# SECRET_KEY = 'django-insecure-e(r*1+g(-39+nc39f&%h9@jd%42zv$q&%dkr+s)$^m!*vwyg_8'
+
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+
+# # Application definition
+# INSTALLED_APPS += []
+
+# # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
