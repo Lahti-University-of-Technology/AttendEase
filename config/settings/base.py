753c63d4de586999539eca7952d4528d070ccf19
Dharmendra Sah <dharmendra11250@gmail.com>
2023-10-11 08:58:34 +0300

    setup static files for styling

--- config/settings/base.py
+++ config/settings/base.py
@@ -65,13 +65,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
-# TEMPLATES_DIR = PROJECT_DIR / 'templates'
-# STATIC_DIR = BASE_DIR / 'static'
+TEMPLATES_DIR = os.path.join(PROJECT_DIR, 'templates/')
+STATIC_DIR = os.path.join(PROJECT_DIR, 'static/')
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, 'templates/')],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
