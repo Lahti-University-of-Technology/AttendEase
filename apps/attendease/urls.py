ac14cba1088d1091e2d06a87ccd9b09723c566ba
Dharmendra Sah <dharmendra11250@gmail.com>
2023-10-18 19:23:11 +0300

    designed dashboard layout

--- apps/attendease/urls.py
+++ apps/attendease/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from attendease import views
-from .views import index
+from .views import index, dashboard
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dashboard/", dashboard, name="dashboard"),
 ]
--- apps/attendease/views.py
+++ apps/attendease/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "attendease/homepage.html")
+
+def dashboard(request):
+    return render(request, "dashboard/base.html")
