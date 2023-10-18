42db049c3cfe6f8cd0b474b0cb555e8c356d7a43
Sivam Prasath <sivamprasathk@gmail.com>
2023-10-18 19:48:42 +0300

    Dashboard Design

--- apps/attendease/urls.py
+++ apps/attendease/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 # from attendease import views
-from .views import index
+from .views import index, practice, lecdashcl, lechistory, schedlec
 
 urlpatterns = [
     path("", index, name="index"),
+    path("practice/", practice, name="practice"),
+    path("lecdashcl/", lecdashcl, name="lecdashcl"),
+    path("lechistory/", lechistory, name="lechistory"),
+    path("schedlec/", schedlec, name="schedlec"),
+    
 ]
--- apps/attendease/views.py
+++ apps/attendease/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "base.html")
+
+def practice(request):
+    return render(request, "practice.html")
+
+def lecdashcl(request):
+    return render(request, "lecdashcl.html")
+
+def lechistory(request):
+    return render(request, "lechistory.html")
+
+def schedlec(request):
+    return render(request, "schedlec.html")
